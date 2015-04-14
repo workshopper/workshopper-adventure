a2d836c8b6f8a84542d0eb52a1d00c1d083219dd
Martin Heidegger <martin.heidegger@gmail.com>
2015-04-15 02:44:36 +0900

    Readable error messages for i18n misuse

--- i18n.js
+++ i18n.js
@@ -2,6 +2,7 @@ const i18n       = require('i18n-core')
     , i18nFs     = require('i18n-core/lookup/fs')
     , i18nObject = require('i18n-core/lookup/object')
     , path       = require('path')
+    , error      = require('./lib/print').error
     , fs         = require('fs')
 
 function i18nChain() {
@@ -76,16 +77,16 @@ function chooseLang (globalDataDir, appDataDir, defaultLang, availableLangs, lan
   }
 
   if (!!lang && typeof lang != 'string')
-    throw new TypeError('Please supply a language. Available languages are: ' + availableLangs.join(', '))
+    return error('Please supply a language. Available languages are: ' + availableLangs.join(', '))
 
   if (lang)
     lang = lang.replace(/_/g, '-').toLowerCase()
 
   if (availableLangs.indexOf(defaultLang) === -1)
-    throw new TypeError('The default language "' + defaultLang + ' is not one of the available languages?! Available languages are: ' + availableLangs.join(', '))
+    return error('The default language "' + defaultLang + ' is not one of the available languages?! Available languages are: ' + availableLangs.join(', '))
 
   if (lang && availableLangs.indexOf(lang) === -1)
-    throw new TypeError('The language "' + lang + '" is not available.\nAvailable languages are ' + availableLangs.join(', ') + '.\n\nNote: the language is not case-sensitive ("en", "EN", "eN", "En" will become "en") and you can use "_" instead of "-" for seperators.')
+    return error('The language "' + lang + '" is not available.\nAvailable languages are ' + availableLangs.join(', ') + '.\n\nNote: the language is not case-sensitive ("en", "EN", "eN", "En" will become "en") and you can use "_" instead of "-" for seperators.')
 
   if (availableLangs.indexOf(data.selected) === -1)
     // The stored data is not available so lets use one of the other languages
