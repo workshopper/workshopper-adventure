8cf01b185a686d49e244ace2503245f4b3851c8b
Martin Heidegger <martin.heidegger@gmail.com>
2016-11-11 05:08:43 +0900

    Added proper separator between content because that makes sense for a footer.

--- default/footer.js
+++ default/footer.js
@@ -1,5 +1,8 @@
 var path = require('path')
 
-module.exports = {
-  file: path.join(__dirname, '../i18n/footer/{lang}.md')
-}
+module.exports = [
+  '---',
+  {
+    file: path.join(__dirname, '../i18n/footer/{lang}.md')
+  }
+]
